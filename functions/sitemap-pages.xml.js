7edbd1c254379ea3106d972b9ed20c61299bbe75
trexxeseba <undiaes@gmail.com>
2026-08-08 12:13:07 -0300

    SEO sitemap: add pages segment

---
+++ functions/sitemap-pages.xml.js
@@ -0,0 +1,18 @@
+import { BASE } from './_shared/catalog.js';
+import { urlsetXml, xmlResponse } from './_shared/sitemap.js';
+
+export const STATIC_SITEMAP_PAGES = Object.freeze([
+  `${BASE}/`,
+  `${BASE}/catalogo`,
+  `${BASE}/libros-maria-montessori-uruguay`,
+  `${BASE}/politicas`,
+  `${BASE}/envios`,
+  `${BASE}/devoluciones`,
+  `${BASE}/terminos`,
+  `${BASE}/privacidad`,
+  `${BASE}/contacto`,
+]);
+
+export async function onRequest() {
+  return xmlResponse(urlsetXml(STATIC_SITEMAP_PAGES.map(loc => ({ loc }))));
+}
