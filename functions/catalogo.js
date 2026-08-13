68a268f302763219023806f2be89c9e38a19ade5
trexxeseba <undiaes@gmail.com>
2026-08-13 19:25:11 -0300

    Alinea la identidad estructurada como tienda online

--- functions/catalogo.js
+++ functions/catalogo.js
@@ -851,7 +851,7 @@ export async function onRequest(ctx) {
             'url':        canonicalHref,
             'description': metaDescription,
             'isPartOf':   { '@type': 'WebSite', 'name': 'Amado Libros', 'url': BASE },
-            'publisher':  { '@type': 'BookStore', 'name': 'Amado Libros', 'url': BASE },
+            'publisher':  { '@type': 'OnlineStore', 'name': 'Amado Libros', 'url': BASE },
           })}</script>`
         : '';
 
